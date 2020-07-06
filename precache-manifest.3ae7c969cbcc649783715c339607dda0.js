633a2033644273d1e9208c7d9d3c51507859f321
KENDRY USUARIO DANIEL <kendrysotomorales@gmail.com>
2020-07-06 00:37:50 -0500

    Updates

---
+++ precache-manifest.3ae7c969cbcc649783715c339607dda0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a094eb7402b732a84f230d08577260d1",
+    "url": "/Rewards-store/index.html"
+  },
+  {
+    "revision": "3dc4a7cfc9dcb12ff2ea",
+    "url": "/Rewards-store/static/css/main.de315909.chunk.css"
+  },
+  {
+    "revision": "c432c0bf6a9814856f7a",
+    "url": "/Rewards-store/static/js/2.0e292eaf.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Rewards-store/static/js/2.0e292eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc4a7cfc9dcb12ff2ea",
+    "url": "/Rewards-store/static/js/main.d14b234f.chunk.js"
+  },
+  {
+    "revision": "277673e629eeb8a13029",
+    "url": "/Rewards-store/static/js/runtime-main.ffbe5917.js"
+  }
+]);
